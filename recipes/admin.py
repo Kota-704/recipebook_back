4a938ff6b0dc8acf910815422eaefff7e360cda6
Kota-704 <kktt14590358@gmail.com>
2025-04-14 21:03:40 +0900

    ✨ 管理画面作成

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import User, Group, Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+  list_display = ('title','user', 'group', 'id')
+  search_fields = ('title',)
+  list_filter = ('group',)
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+  list_display = ('recipe', 'ingredient', 'quantity')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+  list_display = ('name', 'group')
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+  list_display = ('name',)
