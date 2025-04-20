af0b879420f4312ddf18c3529306bc6e5db83f77
Kota-704 <kktt14590358@gmail.com>
2025-04-20 22:51:27 +0900

    ✨ レシピ新規作成機能追加

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RecipeListView
+from .views import RecipeListView, RecipeCreateView
 
 urlpatterns = [
   path('recipes/', RecipeListView.as_view(), name='recipe-list'),
+  path('recipes/create/', RecipeCreateView.as_view(), name='recipe-create'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,7 @@ from .serializers import RecipeSerializer
 class RecipeListView(generics.ListAPIView):
   queryset = Recipe.objects.all()
   serializer_class = RecipeSerializer
+
+class RecipeCreateView(generics.CreateAPIView):
+  queryset = Recipe.objects.all()
+  serializer_class = RecipeSerializer
